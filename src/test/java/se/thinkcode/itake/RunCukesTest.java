681d55247997b3fc2dbea968ff45f2d007525562
brunoguidone <brunoguidone@hotmail.com>
2020-11-28 23:58:15 -0300

    add json

--- src/test/java/se/thinkcode/itake/RunCukesTest.java
+++ src/test/java/se/thinkcode/itake/RunCukesTest.java
@@ -4,5 +4,6 @@ import cucumber.api.junit.Cucumber;
 import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
+@Cucumber.Options(format = {"pretty", "html:target/cucumber", "json:target/cucumber.json"})
 public class RunCukesTest {
 }
